9ba3896d1943146573dd5fef59fba75a98711cc3
edzy143 <65008628+edzy143@users.noreply.github.com>
2020-07-15 10:55:52 +0200

    Fixes

--- timesheets.php
+++ timesheets.php
@@ -39,7 +39,7 @@ $conn = mysqli_connect("localhost", "root", "", "demo");
     </style>
 </head>
 
-<nav class="navbar navbar-inverse">
+<nav class="navbar fixed-top navbar-inverse bg-primary navbar-toggleable-sm"">
   <div class="container-fluid">
     <div class="navbar-header">
     <button type="button" class="navbar-toggle collapsed  visible-xs-block visible-sm-block visible-md-block" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
--- welcome.php
+++ welcome.php
@@ -41,7 +41,7 @@ $conn = mysqli_connect("localhost", "root", "", "demo");
 </head>
 
 
-<nav class="navbar navbar-inverse">
+<nav class="navbar fixed-top navbar-inverse bg-primary navbar-toggleable-sm"">
   <div class="container-fluid">
     <div class="navbar-header">
     <button type="button" class="navbar-toggle collapsed  visible-xs-block visible-sm-block visible-md-block" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
